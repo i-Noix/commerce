d23864a31748a37ee9ab3fe4e39959f67ba9ff86
Roman Nazarov <rnazarov05@gmail.com>
2025-01-24 16:20:32 +0200

    Створює нову модель для списку аукціонів

--- auctions/models.py
+++ auctions/models.py
@@ -5,3 +5,13 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class AuctionListings(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=255)
+    start_bid = models.IntegerField()
+    category = models.CharField(max_length=64)
+    image_url = models.URLField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctions")
+
+    def __str__(self):
+        return self.title
